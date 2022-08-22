9b12f7b93a35da9b4cc120925668d70a0fba0929
TetianaPiatykh <tanya.pjatykh@gmail.com>
2022-08-22 19:29:18 +0300

    2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
 
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+let timerId = null;
+
+
+startBtn.addEventListener('click', changeColor);
+stopBtn.addEventListener('click', buttonStop);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+function changeColor(){
+    timerId = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    startBtn.disabled = true;
+};
+
+
+function buttonStop(){
+    clearInterval(timerId);
+     startBtn.disabled = false;
+};
+
